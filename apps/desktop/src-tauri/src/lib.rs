fd231e8aa8ed8567aa4c5830b5ef4fdeacd556f5
MG-feng <fengmelvina@gmail.com>
2026-09-15 16:46:34 +0800

    Update lib.rs

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -8,22 +8,29 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_deep_link::init())
         .setup(|app| {
-            let app_data_dir = app.path().app_data_dir().unwrap_or_else(|_| std::path::PathBuf::from("."));
-            let plugin_manager = core::plugin_manager::PluginManager::new(app_data_dir);
+            let app_data_dir = app.path().app_data_dir().expect("app data dir required");
+            let plugin_manager = core::plugin_manager::PluginManager::new(app_data_dir.clone());
+            
             app.manage(AppState { plugin_manager: std::sync::Mutex::new(plugin_manager) });
+            app.manage(commands::auth::AuthState::new());
+
+            #[cfg(any(target_os = "linux", all(debug_assertions, windows)))]
+            {
+                use tauri_plugin_deep_link::DeepLinkExt;
+                let _ = app.deep_link().register_all();
+            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
-            commands::plugin::plugin_list,
-            commands::plugin::plugin_install,
-            commands::plugin::plugin_uninstall,
-            commands::plugin::plugin_toggle,
-            commands::settings::settings_get,
-            commands::settings::settings_set,
-            commands::settings::settings_select_directory,
-            commands::settings::settings_get_cache_size,
+            commands::plugin::plugin_list, commands::plugin::plugin_install_via_dialog,
+            commands::plugin::plugin_uninstall, commands::plugin::plugin_toggle,
+            commands::settings::settings_get, commands::settings::settings_set,
+            commands::settings::settings_select_directory, commands::settings::settings_get_cache_size,
             commands::settings::settings_clear_cache,
+            commands::auth::auth_login, commands::auth::auth_callback, commands::auth::auth_get_state, commands::auth::auth_logout,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

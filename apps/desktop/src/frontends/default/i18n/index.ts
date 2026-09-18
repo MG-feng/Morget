631ac536fcaedd9fd912c293506aef4d55f2e047
MGFeng <fengmelvina@gmail.com>
2026-09-18 14:50:56 +0800

    Create index.ts

---
+++ apps/desktop/src/frontends/default/i18n/index.ts
@@ -0,0 +1,9 @@
+import zhTW from './zh-TW';
+import zhCN from './zh-CN';
+import enUS from './en-US';
+
+export const locales: Record<string, Record<string, string>> = {
+  'zh-TW': zhTW,
+  'zh-CN': zhCN,
+  'en-US': enUS,
+};

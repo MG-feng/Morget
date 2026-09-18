9465a0296f2400990a67966b41fb1a8a1b1e62cf
MGFeng <fengmelvina@gmail.com>
2026-09-18 15:21:50 +0800

    Update main.tsx

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { I18nProvider } from './frontends/default/I18nProvider.tsx';
 import { MorgetDialogProvider } from './components/MorgetDialog.tsx';
-import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
